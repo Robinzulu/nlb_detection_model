a4695861ccfe9a1c6fb064e926e5ed30abf34397
Robin_Mutai <113591484+Robinzulu@users.noreply.github.com>
2023-10-18 21:34:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -42,39 +42,31 @@ st.markdown(
 st.title("NLB Maize Detection")
 st.write("Detect Northern Leaf Blight (NLB) in maize plants from images.")
 
-# Instructions with tooltips
+# Instructions
 st.header("Instructions")
 st.markdown("1. Upload an image of a maize leaf.")
 st.markdown("2. We will analyze the image and provide you with the result.")
 st.markdown("3. If your plants are unhealthy, we recommend some fertilizers for you.")
 
-# Upload image with error handling
+# Upload image
 uploaded_image = st.file_uploader("Upload an image of a maize leaf", type=["jpg", "jpeg", "png"])
-if uploaded_image is None:
-    st.warning("Please upload a valid image.")
 
-else:
+if uploaded_image is not None:
     # Display uploaded image
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
     image = Image.open(uploaded_image)
     preprocessed_image = preprocess_image(image)
 
-    # Perform NLB detection with progress feedback
-    with st.spinner("Please wait, analyzing the image..."):
+    # Perform NLB detection
+    with st.spinner("Analyzing..."):
         prediction = model.predict(np.expand_dims(preprocessed_image, axis=0))
         prediction_probability = prediction[0][0]
 
-    st.header("Prediction Result")
-
-    # Scale prediction_probability to be between 0.0 and 1.0
-    scaled_prediction_probability = max(0.0, min(1.0, prediction_probability))
-
-    # Visualize the result with a confidence level meter
-    st.progress(scaled_prediction_probability)
+    st.header(f"Prediction Probability: {prediction_probability:.2f}")
 
     # Provide recommendations based on the prediction
-    if scaled_prediction_probability > 0.5:
+    if prediction_probability > 0.5:
         st.warning("Your plants may be unhealthy. Consider taking the following steps:")
         st.markdown("- Consult with an agricultural expert.")
         st.markdown("- Apply appropriate treatments.")
@@ -85,14 +77,6 @@ else:
         st.markdown("- Follow fertilization recommendations.")
         
         st.header("Recommended Fertilizers:")
-
-        # Adding a link with legal considerations
         st.markdown("- [Booster Foliar Fertilizer 1Ltr](https://cheapthings.co.ke/product/booster-foliar-fertilizer-1ltr/?gad=1&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D59O3FXxJTZkvS9UTNG8iNWSBqVuQ6DNVfmrVQNTImX0ohgp80AX1qIaAvlJEALw_wcB)")
 
-# Additional user feedback and disclaimer
-st.markdown(
-    """
-    **Disclaimer:** This tool provides general recommendations. For specific advice, consult with an agricultural expert.
-    Feel free to provide [feedback](mailto:your@email.com) on this tool's performance.
-    """
-)
+#st.write("Upload an image to detect NLB in maize leaves.")
