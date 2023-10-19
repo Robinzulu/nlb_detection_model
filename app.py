405da7a618b550b97bf24b2de5160ccfc9403328
Robin_Mutai <113591484+Robinzulu@users.noreply.github.com>
2023-10-19 11:06:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,9 @@ st.write("Detect Northern Leaf Blight (NLB) in maize plants from images.")
 
 # Instructions with additional recommendations and photo requirements
 st.header("Instructions")
-st.markdown("1. Upload an image of a maize leaf or use your device's camera to capture one.")
+st.markdown("1. Choose an option to provide an image for NLB detection:")
+st.markdown("   - Option 1: Upload an image of a maize leaf.")
+st.markdown("   - Option 2: Use your device's camera to capture an image.")
 st.markdown("2. We will analyze the image and provide you with the result.")
 st.markdown("3. For accurate results, make sure your photo meets these criteria:")
 st.markdown("   - The photo should not contain too many leaves; focus on a single leaf or a few leaves.")
@@ -68,30 +70,30 @@ class VideoTransformer(VideoTransformer):
         prediction_probability = perform_nlb_detection(image)
         return f"Prediction Probability: {prediction_probability:.2f}"
 
-# Display uploaded image or use the device's camera
+# Option to upload an image
+st.header("Option 1: Upload an Image")
 uploaded_image = st.file_uploader("Upload an image of a maize leaf", type=["jpg", "jpeg", "png"])
+
+# Option to capture an image from the camera
+st.header("Option 2: Capture Image from Camera")
+webrtc_streamer(
+    key="camera",
+    video_transformer_factory=VideoTransformer,
+)
+
+# Perform NLB detection based on user choice
 if uploaded_image is not None:
-    # Display uploaded image
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
     image = Image.open(uploaded_image)
     st.header(perform_nlb_detection(image))
-else:
-    st.header("Capture Image from Camera")
-    webrtc_streamer(
-        key="camera",
-        video_transformer_factory=VideoTransformer,
-    )
-
-# Provide recommendations based on the prediction
-if "camera" not in st.session_state:
-    if st.session_state.camera is not None:
-        if st.session_state.camera > 0.5:
-            st.warning("Your plants may be unhealthy. Consider taking the following steps:")
-            st.markdown("- Consult with an agricultural expert.")
-            st.markdown("- Apply appropriate treatments.")
-        else:
-            st.success("Your maize plants appear to be healthy. Here are some tips for maintaining their well-being:")
-            st.markdown("- Maintain a proper watering schedule.")
-            st.markdown("- Follow fertilization recommendations.")
-            st.header("Recommended Fertilizers:")
-            st.markdown("- [Booster Foliar Fertilizer 1Ltr](https://cheapthings.co.ke/product/booster-foliar-fertilizer-1ltr/?gad=1&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D59O3FXxJTZkvS9UTNG8iNWSBqVuQ6DNVfmrVQNTImX0ohgp80AX1qIaAvlJEALw_wcB)")
+elif "camera" in st.session_state and st.session_state.camera is not None:
+    if st.session_state.camera > 0.5:
+        st.warning("Your plants may be unhealthy. Consider taking the following steps:")
+        st.markdown("- Consult with an agricultural expert.")
+        st.markdown("- Apply appropriate treatments.")
+    else:
+        st.success("Your maize plants appear to be healthy. Here are some tips for maintaining their well-being:")
+        st.markdown("- Maintain a proper watering schedule.")
+        st.markdown("- Follow fertilization recommendations.")
+        st.header("Recommended Fertilizers:")
+        st.markdown("- [Booster Foliar Fertilizer 1Ltr](https://cheapthings.co.ke/product/booster-foliar-fertilizer-1ltr/?gad=1&gclid=Cj0KCQjwhL6pBhDjARIsAGx8D59O3FXxJTZkvS9UTNG8iNWSBqVuQ6DNVfmrVQNTImX0ohgp80AX1qIaAvlJEALw_wcB)")
